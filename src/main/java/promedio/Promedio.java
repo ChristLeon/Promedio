fdad889e0ce030adc9ec2b1efb0a586686299356
Cristopher <Cristopher@192.168.1.4>
2022-11-11 19:13:02 -0500

    promedio

--- src/main/java/promedio/Promedio.java
+++ src/main/java/promedio/Promedio.java
@@ -6,20 +6,20 @@ public class Promedio {
     double suman=0;
     double promf=0;
     
-    System.out.println("Ingrese la cantidad de las cuales desea hallar su promedio: ");
+    System.out.println("Ingrese la cantidad de los numeros de los cuales desea hallar su promedio: ");
     int prom=scn.nextInt();
     
     double notas[]=new double [prom];
     
     for(int i = 0; i <notas.length; i++){
-    System.out.println("Ingrese Nota:"+(i+1));
+    System.out.println("Ingrese el numero entero:"+(i+1));
     notas[i]=scn.nextDouble();
     }
     for(int i = 0; i <notas.length; i++){
     suman=suman+notas[i];
     promf=suman/prom;
     }
-        System.out.println("Promedio Final: "+promf);
+        System.out.println("El promedio final de los numeros enteros: "+promf);
     }
     
 }
